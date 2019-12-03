c9f4d6106be6cb418b8555f759731ac50e534b46
Abe Baali <milkyabdullah@gmail.com>
2019-12-02 22:56:38 -0600

    piano ui

--- App.js
+++ App.js
@@ -1,19 +1,96 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { ScreenOrientation } from "expo";
+import { StyleSheet, Text, View } from "react-native";
+import sources from "./audio";
+
+const sharps = [1, 3, 6, 8, 10];
+const keys = [...Array(sources.length).keys()].filter(k => !sharps.includes(k));
+const ogColors = [...Array(sources.length).keys()].map(a =>
+	sharps.includes(a) ? "black" : "white"
+);
+
+const KEY_HEIGHT = 160;
+const KEY_WIDTH = 48;
+const SHARP_HEIGHT = 100;
+const SHARP_WIDTH = 32;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [colors, setColors] = useState(ogColors);
+
+	useEffect(() => {
+		ScreenOrientation.lockAsync(
+			ScreenOrientation.OrientationLock.LANDSCAPE_LEFT
+		);
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<View
+				style={{
+					position: "relative",
+					flexDirection: "row",
+					alignItems: "center",
+					justifyContent: "center",
+					borderTopWidth: 1,
+					borderRightWidth: 1
+				}}
+			>
+				{keys.map(key => (
+					<View
+						onTouchStart={() => {
+							setColors(colors.map((c, i) => (i == key ? "#dadada" : c)));
+						}}
+						onTouchEnd={() => {
+							setColors(ogColors);
+						}}
+						style={{
+							backgroundColor: colors[key],
+							height: KEY_HEIGHT,
+							width: KEY_WIDTH,
+							borderBottomWidth: 1,
+							borderLeftWidth: 1
+						}}
+					></View>
+				))}
+				<View
+					style={{
+						position: "absolute",
+						top: 0,
+						left: 0
+					}}
+				>
+					{sharps.map((key, i) => (
+						<View
+							onTouchStart={() => {
+								setColors(colors.map((c, i) => (i == key ? "#4e4e4e" : c)));
+							}}
+							onTouchEnd={() => {
+								setColors(ogColors);
+							}}
+							style={{
+								backgroundColor: colors[key],
+								position: "absolute",
+								top: 0,
+								left:
+									keys.filter(k => k < key).length * KEY_WIDTH -
+									SHARP_WIDTH / 2,
+								height: SHARP_HEIGHT,
+								width: SHARP_WIDTH
+							}}
+						></View>
+					))}
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#F5FCFF",
+		flexDirection: "row"
+	}
 });
